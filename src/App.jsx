2673911b616d213cb792450feaa5b22c3cc5113f
4llves <jhonatab4@gmail.com>
2023-07-10 23:58:26 -0300

    add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-function App() {
+import { Post } from './Post'
+import { Header } from './components/Header'
+
+import './styles.css'
+
+export function App() {
 
   return (
-    <>
-      <h1>Hello World </h1>
-    </>
+    <div>
+      <Header />
+
+      <Post
+        author="Alves"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur alias incidunt tempora dignissimos nisi nemo quaerat quam temporibus hic, laborum esse nulla mollitia necessitatibus explicabo ex error earum, numquam rem."
+      />
+    </div>
   )
 }
 
-export default App
