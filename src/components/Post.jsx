27a437823032490486719484e1c65a8ec240fd1d
4llves <jhonatab4@gmail.com>
2023-07-12 23:54:24 -0300

    add focus-within in button publish

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -39,6 +39,16 @@ export function Post() {
           </p>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu comentário</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
