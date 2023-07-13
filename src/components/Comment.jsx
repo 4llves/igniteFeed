b11e9cd91dcb176501edada78e4f2f2a09f010fd
4llves <jhonatab4@gmail.com>
2023-07-13 00:12:49 -0300

    add component comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/4llves.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Alves Jhonata</strong>
+
+              <time title="11 de maio às 08:13h" dateTime="2023-07-12">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <ThumbsUp /> Aplaudir <span>20</span>
+        </footer>
+      </div>
+    </div>
+  )
+}
