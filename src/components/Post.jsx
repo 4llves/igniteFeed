9877b18bc5c810e05ef4acaad345d7da2c66fcad
4llves <jhonatab4@gmail.com>
2023-07-17 22:36:56 -0300

    add page responsive

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -22,20 +22,18 @@ export function Post() {
       </header>
 
       <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
         <p>
-          <p>Fala galeraa 👋</p>
-          <p>
-            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
-            🚀
-          </p>
-          <p>
-            👉 <a href="#">jane.design/doctorcare</a>
-          </p>
-          <p>
-            <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{' '}
-            <a href="#">#rocketseat</a>
-          </p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{' '}
+          <a href="#">#rocketseat</a>
         </p>
       </div>
 
